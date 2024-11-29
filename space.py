568bef01bafca280886d76a6d07a7d5001353b21
Travis Virgil <travisjvirgil@gmail.com>
2024-11-29 13:00:06 -0500

    Create spaces.

---
+++ space.py
@@ -0,0 +1,14 @@
+class Space:
+    def _init_(self, name):
+        self.name = name
+        self.connected_spaces = []
+
+    def add_connection(self, space):
+        if space not in self.connected_spaces:
+            self.connected_spaces.append((space))
+    
+    def get_connections(self):
+        return self.connected_spaces
+    
+    def _repr_(self):
+        return f"Space({self.name})"
