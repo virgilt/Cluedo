5de55d0cfd98a0542fef49c4d6e50b7ec5661880
Travis Virgil <travisjvirgil@gmail.com>
2024-11-30 21:22:05 -0500

    Update player names.

--- game.py
+++ game.py
@@ -3,6 +3,6 @@ from game_manager import GameManager
 if __name__ == "__main__":
     game = GameManager()
     
-    game.setup_game(["Player 1", "Player 2", "Player 3"])
+    game.setup_game(["P1", "P2", "P3"])
     
     game.start_game()
